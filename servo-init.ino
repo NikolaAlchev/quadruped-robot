fdfa90d475ba32207f776c3ca085c9d22a5c7309
NikolaAlchev <107628102+NikolaAlchev@users.noreply.github.com>
2024-10-11 18:06:00 +0200

    Added initial files

---
+++ servo-init.ino
@@ -0,0 +1,38 @@
+// Locate the initial position of legs 
+// RegisHsu 2015-09-09
+
+// Wire - Version: Latest 
+#include <Wire.h>
+
+// Adafruit PWM Servo Driver Library - Version: Latest 
+#include <Adafruit_PWMServoDriver.h>
+
+
+//define servos' ports
+const int servo[4][3] = { { 13, 14, 12 }, { 5, 6, 4 }, { 9, 10, 8 }, { 1, 2, 0 } };
+#define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+  
+void setup()
+{
+  //initialize all servos
+  pwm.begin();
+  
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+
+  delay(10);
+}
+
+void loop(void)
+{
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      pwm.setPWM(servo[i][j], 0, map(90, 0, 180, SERVOMIN, SERVOMAX));
+      delay(20);
+    }
+  }
+}
